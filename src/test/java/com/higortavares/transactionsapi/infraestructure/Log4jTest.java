65c283ad0941c3e8cb73aea26f484b386a9f8c9f
paulo.tavares <paulo.tavares@olxbr.com>
2023-12-19 22:50:26 -0300

    Include test dir on checkstyle validations

--- src/test/java/com/higortavares/transactionsapi/infraestructure/Log4jTest.java
+++ src/test/java/com/higortavares/transactionsapi/infraestructure/Log4jTest.java
@@ -1,18 +1,20 @@
 package com.higortavares.transactionsapi.infraestructure;
 
-import org.apache.logging.log4j.Logger;
+
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 class Log4jTest {
   private static final Logger log = LogManager.getLogger("HelloWorld");
+
   @Test
   void shouldLogHello() {
     log.info("Hello world!");
   }
 
   @Test
-  void shouldLogError(){
+  void shouldLogError() {
     log.error("FAILED SUCCESSFULLY");
   }
 
